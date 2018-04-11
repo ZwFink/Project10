c8ce9700c48fb6d7100f70f841d4fbb79fe9892d
Bob Costas <finkzane@gmail.com>
2018-04-10 19:16:38 -0700

    Added static member variables

--- GenericHashClass.java
+++ GenericHashClass.java
@@ -5,7 +5,33 @@ package p10_Package;
  *
  * @author Zane Fink
  */
-public class GenericHashClass
+public class GenericHashClass< GenericData extends  Comparable< GenericData > >
 {
+    /**
+     * Default number of characters of data string to use in hash calculation
+     */
+    private static final int DEFAULT_NUM_HASH_DIGITS = 6;
+
+
+    /**
+     * Table size default
+     */
+    private static final int DEFAULT_TABLE_SIZE = 10;
+
+    /**
+     * Constant for returning item not found with search
+     */
+    private static final int ITEM_NOT_FOUND = -1;
+
+    /**
+     * Constant for setting linear probing
+     */
+    public static final int LINEAR_PROBING = 101;
+
+    /**
+     * Constant for setting quadratic probing
+     */
+    public static final int QUADRATIC_PROBING = 102;
+
 
 }
